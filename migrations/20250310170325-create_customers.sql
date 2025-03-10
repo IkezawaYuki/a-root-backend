99a3260d8d29fa5122a4bd31e49ec92fcfb7076d
cover-ikezawa <ikezawa.yuki@cover-corp.com>
2025-03-10 18:21:31 +0900

    mod

--- migrations/20250310170325-create_customers.sql
+++ migrations/20250310170325-create_customers.sql
@@ -1,4 +1,27 @@
 
 -- +migrate Up
+CREATE TABLE IF NOT EXISTS `customers` (
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `wordpress_url` VARCHAR(255) DEFAULT NULL,
+    `start_date` DATETIME DEFAULT NULL,
+    `facebook_token` VARCHAR(255) DEFAULT NULL,
+    `instagram_token_status` INT NOT NULL DEFAULT 0,
+    `instagram_account_id` VARCHAR(255) DEFAULT NULL,
+    `instagram_account_name` VARCHAR(255) DEFAULT NULL,
+    `stripe_subscription_id` VARCHAR(255) DEFAULT NULL,
+    `stripe_customer_id` VARCHAR(255) DEFAULT NULL,
+    `payment_type` INT NOT NULL DEFAULT 0,
+    `payment_status` INT NOT NULL DEFAULT 0,
+    `delete_hash_flag` INT NOT NULL DEFAULT 0,
+    `dashboard_status` INT NOT NULL DEFAULT 0,
+    `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `deleted_at` timestamp DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- +migrate Down
+DROP TABLE IF EXISTS `customers`;
---
+++ migrations/20250310181656-create_posts.sql
@@ -0,0 +1,17 @@
+
+-- +migrate Up
+CREATE TABLE IF NOT EXISTS `customers` (
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `customer_id` BIGINT UNSIGNED NOT NULL,
+    `instagram_media_id` VARCHAR(255) NOT NULL,
+    `instagram_link` TEXT NOT NULL,
+    `wordpress_media_id` VARCHAR(255) NOT NULL,
+    `wordpress_link` TEXT NOT NULL,
+    `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `deleted_at` timestamp DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- +migrate Down
+DROP TABLE IF EXISTS `customers`;
